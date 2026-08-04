5b142d4092376ce70cbaf97c94c2a8191356a89e
Navini Ariyasinghe <kari487@aucklanduni.ac.nz>
2026-08-05 00:19:02 +1200

    Added in descriptions for the projects I did

---
+++ src/data/projects.js
@@ -0,0 +1,47 @@
+const projects = [
+    {
+        id: 1, 
+        name: "ECSquare", 
+        tech: "HTML/CSS • JavaScript",
+        description: "Winner of the Student's Choice Award for the ECSE Design Competition 2026. A piano tiles game, which uses a custom-made keyboard to play songs, like the real Piano Tiles game.",
+        images: [],
+
+    },
+    {
+        id: 2, 
+        name: "Snoot",
+        tech: "Kotlin • Figma • Android Studio",
+        description: "Winner of Best Design at WDCC x SESA Hackathon 2026. Snoot is designed to decrease screentime, where the decisions you make, such as choosing to be productive or doomscrolling, will impact your Pet and it's health.",
+         images: [],
+    },
+    {
+        id: 3, 
+        name: "Portfolio Website - Navini Creates",
+        tech: "HTML/CSS • React • JavaScript • Figma",
+        description: "My personal portfolio that I designed and developed!",
+        images: [],
+    },
+    {
+        id: 4,
+        name: "Trial AI",
+        tech: "Java • JavaFX • Prompt Engineering • CSS",
+        description: "A court-room simulation of an AI on trial for a copyright breach, at their workplace.",
+        images: [],
+    },
+    {
+        id: 5,
+        name: "Activity Operator",
+        tech: "Java",
+        description: "A system which manages and organises Operators and their respective Activities, as well as managing Reviews.",
+        images: [],
+    },
+    {
+        id: 6,
+        name: "Mind Game",
+        tech: "Java",
+        description: "A Java game where you are playing against AI, with the aim to guess the colour that the AI is thinking of.",
+        images: [],
+    }
+];
+
+export default projects;
