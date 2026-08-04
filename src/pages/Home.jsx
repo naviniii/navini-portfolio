8df15a3e8f01eefb1f300857f44aef6dd1fef17e
Navini Ariyasinghe <kari487@aucklanduni.ac.nz>
2026-08-04 23:35:24 +1200

    Finalised the design and features of the Home Page, Home Page CSS and added background colour to Index.css to prevent odd spots from showing when scrolling too far up/down

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import Flower from '../components/Flower';
+import Character from '../components/Character';
+import './Home.css';
+
+
 function Home(){
-    return <h1> Home Page </h1>;
+    return(
+        <section className = "hero">
+            <div className = "hero-content">
+                <h1 className = "hero-name"> navini ariyasinghe </h1>
+                <p className = "hero-est"> est. 2026</p>
+                <p className = "hero-welcome-sign"> Welcome to my little world &hearts;</p>
+            </div>
+
+            <div className = "hero-scene">
+                <Flower to = "/projects" label = "projects" variant = {1}/>
+                <Character to = "/about" label = "about me"/>
+                <Flower to = "/diary" label = "my digital diary" variant = {2} />
+            </div>
+        </section>
+    );
 
 }
 
