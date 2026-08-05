3c62e8531ce44b84edf38acf5d97d07b8807b143
Navini Ariyasinghe <kari487@aucklanduni.ac.nz>
2026-08-05 23:28:47 +1200

    Adding in the Projects Page, and adjusting the CSS. Still needs some work

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,45 @@
+import { useState } from 'react';
+import Flower from '../components/Flower';
+import ProjectView from '../components/ProjectView';
+import projects from '../data/projects';
+import './Projects.css';
+
+
 function Projects(){
-    return <h1> Project Page </h1>;
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const handleFlowerClick = (project) => {
+        if (selectedProject?.id === project.id){
+            setSelectedProject(null); 
+
+        }else{
+            setSelectedProject(project); 
+        }
+    };
+
+    return (
+        <section className = "projects-hero">
+            <h1 className = "projects-title"> Projects </h1>
+            <div className = "projects-garden">
+                {projects.map((project,i) => (
+                    <div key = {project.id} className = {`project-flower ${selectedProject?.id === project.id ? 'active' : ''}`} 
+                    /*explanation of code for future me: 
+                    - ?. handles null 
+                    - ` is for template literal - which is a string with JS expressions 
+                    - the ? active is for shorthanding if condition is true use active or otherwise use empty string
+                    
+                    */
+                    onClick = {() => handleFlowerClick(project)}>
+                        <Flower to = "#" label = {project.name} variant = {( i % 3 ) + 1} />
+                        </div>
+                ))}
+            </div>
+
+            {selectedProject && (<ProjectView project = {selectedProject} /> )}
+        </section>
+
+    );
+
 
 }
 
