cbee78418617cdde58af3f478378e92644c9b339
Navini Ariyasinghe <kari487@aucklanduni.ac.nz>
2026-08-05 23:28:19 +1200

    Adding in the photos with the relevant project.

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
         name: "ECSquare", 
         tech: "HTML/CSS • JavaScript",
         description: "Winner of the Student's Choice Award for the ECSE Design Competition 2026. A piano tiles game, which uses a custom-made keyboard to play songs, like the real Piano Tiles game.",
-        images: [],
+        images: ["/projects/ecsquare_1.png","/projects/ecsquare_2.png","/projects/ecsquare_3.png"],
 
     },
     {
@@ -12,35 +12,35 @@ const projects = [
         name: "Snoot",
         tech: "Kotlin • Figma • Android Studio",
         description: "Winner of Best Design at WDCC x SESA Hackathon 2026. Snoot is designed to decrease screentime, where the decisions you make, such as choosing to be productive or doomscrolling, will impact your Pet and it's health.",
-         images: [],
+         images: ["/projects/snoot_1.png","/projects/snoot_2.png","/projects/snoot_3.png",],
     },
     {
         id: 3, 
-        name: "Portfolio Website - Navini Creates",
+        name: "Portfolio Website",
         tech: "HTML/CSS • React • JavaScript • Figma",
         description: "My personal portfolio that I designed and developed!",
-        images: [],
+        images: ["/projects/portfolio.png"],
     },
     {
         id: 4,
         name: "Trial AI",
         tech: "Java • JavaFX • Prompt Engineering • CSS",
         description: "A court-room simulation of an AI on trial for a copyright breach, at their workplace.",
-        images: [],
+        images: ["/projects/trialAI_1.png","/projects/trialAI_2.png"],
     },
     {
         id: 5,
         name: "Activity Operator",
         tech: "Java",
         description: "A system which manages and organises Operators and their respective Activities, as well as managing Reviews.",
-        images: [],
+        images: ["/projects/activity_op.png"],
     },
     {
         id: 6,
         name: "Mind Game",
         tech: "Java",
         description: "A Java game where you are playing against AI, with the aim to guess the colour that the AI is thinking of.",
-        images: [],
+        images: ["/projects/mindGame.png"],
     }
 ];
 
