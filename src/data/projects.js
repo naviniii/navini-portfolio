0366aa3ad5e0b595d4a0a4b03fc5813590f07302
Navini Ariyasinghe <kari487@aucklanduni.ac.nz>
2026-08-22 00:24:55 +1200

    Changed every image in Projects section to webp, to see if the images would load faster. The images are now very small (between 10-100kb) so should have significant reduced loading times. Kept old png images in folder delete_if_uneeded, incase this goes wrong

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
         name: "ECSquare", 
         tech: "HTML/CSS • JavaScript",
         description: "Winner of the Student's Choice Award for the ECSE Design Competition 2026. A piano tiles game, which uses a custom-made keyboard to play songs, like the real Piano Tiles game.",
-        images: ["/projects/ecsquare_1.png","/projects/ecsquare_2.png","/projects/ecsquare_3.png"],
+        images: ["/projects/ecsquare_1.webp","/projects/ecsquare_2.webp","/projects/ecsquare_3.webp"],
 
     },
     {
@@ -12,35 +12,35 @@ const projects = [
         name: "Snoot",
         tech: "Kotlin • Figma • Android Studio",
         description: "Winner of Best Design at WDCC x SESA Hackathon 2026. Snoot is designed to decrease screentime, where the decisions you make, such as choosing to be productive or doomscrolling, will impact your Pet and it's health.",
-         images: ["/projects/snoot_1.png","/projects/snoot_2.png","/projects/snoot_3.png",],
+         images: ["/projects/snoot_1.webp","/projects/snoot_2.webp","/projects/snoot_3.webp",],
     },
     {
         id: 3, 
         name: "Portfolio Website",
         tech: "HTML/CSS • React • JavaScript • Figma",
         description: "My personal portfolio that I designed and developed! Every element on this website was designed by me, whether it was drawn or made on Canva",
-        images: ["/projects/portfolio.png"],
+        images: ["/projects/portfolio.webp"],
     },
     {
         id: 4,
         name: "Trial AI",
         tech: "Java • JavaFX • Prompt Engineering • CSS",
         description: "A court-room simulation of an AI on trial for a copyright breach, at their workplace.",
-        images: ["/projects/trialAI_1.png","/projects/trialAI_2.png"],
+        images: ["/projects/trialAI_1.webp","/projects/trialAI_2.webp"],
     },
     {
         id: 5,
         name: "Activity Operator",
         tech: "Java",
         description: "A system which manages and organises Operators and their respective Activities, as well as managing Reviews.",
-        images: ["/projects/activity_op.png"],
+        images: ["/projects/activity_op.webp"],
     },
     {
         id: 6,
         name: "Mind Game",
         tech: "Java",
         description: "A Java game where you are playing against AI, with the aim to guess the colour that the AI is thinking of.",
-        images: ["/projects/mindGame.png"],
+        images: ["/projects/mindGame.webp"],
     }
 ];
 
